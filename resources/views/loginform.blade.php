8130d6d1a12c858d166f8d41c41edd4f3eec987b
VGD <9024841@student.zadkine.nl>
2026-01-19 15:48:59 +0100

    vgd first

---
+++ resources/views/loginform.blade.php
@@ -0,0 +1,8 @@
+test
+<form action="method">
+    <input type="email">
+    <input type="password">
+    <input type="checkbox">
+    <input type="text">
+    <input type="text">
+</form>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth');
+});//->middleware('auth');
+
+Route::get("/login", function(){
+    return view("loginform");
+});
